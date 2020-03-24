078292a13f036c67a040fb31ba530cf5709d0b28
Jorge Gomez <jorge6242@gmail.com>
2020-03-24 15:08:54 -0400

    change the env

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AXIOS = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://club.api.com',
   headers: {
     'Content-Type': 'application/json',
   },
