c95e5fdc5994275bb453b725bc81d426fc5f8a85
Jorge Gomez <jorge6242@gmail.com>
2020-03-30 09:14:32 -0400

    host api

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AXIOS = axios.create({
-  baseURL: 'http://localhost:8080/controlsocios/qa/api/',
+  baseURL: 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
   },
