392dcc4fc81e731283865605be80dae040d99cc2
Jorge Gomez <jorge6242@gmail.com>
2020-03-30 05:55:30 -0400

    config axios

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AXIOS = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://localhost:8080/controlsocios/api/',
   headers: {
     'Content-Type': 'application/json',
   },
--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -5,7 +5,9 @@ interface HeadersData {
 }
 
 export default function headers() {
-    const items: HeadersData = { 'Content-Type': 'application/json' };
+    const items: HeadersData = { 
+      'Content-Type': 'application/json',
+    };
     const token = SecureStorage.getItem('token');
     if (token) {
       items.Authorization = `Bearer ${token}`;
