a6549a929044551b0b45d37ae149e5c38a1585b4
Jorge Gomez <jorge6242@gmail.com>
2020-03-30 07:20:34 -0400

    axios config

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const AXIOS = axios.create({
-  baseURL: 'http://localhost:8080/controlsocios/api/',
+  baseURL: 'http://localhost:8080/controlsocios/qa/api/',
   headers: {
     'Content-Type': 'application/json',
     'Partners-Application': 'admin'
