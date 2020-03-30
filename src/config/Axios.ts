fa63d5e7929d479e98d31b6ecb20d60db9cd32fb
Jorge Gomez <jorge6242@gmail.com>
2020-03-30 06:18:12 -0400

    config axios

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -4,6 +4,7 @@ const AXIOS = axios.create({
   baseURL: 'http://localhost:8080/controlsocios/api/',
   headers: {
     'Content-Type': 'application/json',
+    'Partners-Application': 'admin'
   },
   timeout: 20000,
 });
