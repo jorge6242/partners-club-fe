639f8b5f33709fb5a5d4de3bd1a0ce3ac2f851df
Jorge Gomez <jorge6242@gmail.com>
2020-04-12 16:22:34 -0400

    fixes

--- src/actions/personActions.ts
+++ src/actions/personActions.ts
@@ -26,7 +26,9 @@ export const getAll = (page: number = 1, perPage: number = 8) => async (
         total: data.total,
         perPage: data.per_page,
         prevPageUrl: data.prev_page_url,
-        currentPage: data.current_page
+        currentPage: data.current_page,
+        from: data.from,
+        to: data.to
       };
       response = data.data;
       dispatch({
