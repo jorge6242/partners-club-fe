682524ae9b33b19639dc492e86f8b11f73b094d2
Jorge Gomez <jorge6242@gmail.com>
2020-04-29 12:45:00 -0400

    QA axios config

--- src/config/Axios.ts
+++ src/config/Axios.ts
@@ -12,7 +12,7 @@ import axios from 'axios';
 //http://192.168.0.252:9002
 
 const AXIOS = axios.create({
-  baseURL: 'http://club.api.com',
+  baseURL: 'http://192.168.0.252:9002',
   headers: {
     'Partners-Application': 'admin'
     },
